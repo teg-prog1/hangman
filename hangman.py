f2ad6327e7c61487e8f65b7967ab275e5359c25b
Michael Hemph <michael.hemph@gmail.com>
2023-11-05 12:22:32 +0100

    Kontrollera om rätt bokstav

--- hangman.py
+++ hangman.py
@@ -1,6 +1,9 @@
 # Skapa ett exempelord
 word = "programmering"
 
+# Skapa en lista för redan gissade bokstäver
+guessed = []
+
 # Fortsätt tills vi bryter på annat sätt
 while True:
     while True:
@@ -12,4 +15,12 @@ while True:
         else:
             print("Felaktig inmatning, försök igen.")
 
+    # Kontrollera först så att det inte är en gammal gissning
+    if letter in guessed:
+        print("Den bokstaven har du redan gissat på.")
+    elif letter in word:
+        print("Bra jobbat! Bokstaven ingår i ordet.")
+    else:
+        print("Tyvärr! Den bokstaven ingår inte i ordet.")
+    
 
