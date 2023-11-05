2b2599b628b370e8509e58883f302eebe2350ba2
Michael Hemph <michael.hemph@gmail.com>
2023-11-05 12:12:52 +0100

    Skapa programstruktur och be användaren om en gissning

---
+++ hangman.py
@@ -0,0 +1,7 @@
+# Skapa ett exempelord
+word = "programmering"
+
+# Fortsätt tills vi bryter på annat sätt
+while True:
+    # Be användaren om en bokstav
+    letter = input("Gissa på en bokstav: ")
